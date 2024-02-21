e4948429b41ec7e9af00c38ac88fd5dbd9b8b60a
git-user-cpp <akushyk799@proton.me>
2024-02-21 15:49:58 +0200

    week2_8

--- src/rust_fundamentals/week2/match_control_flow/src/main.rs
+++ src/rust_fundamentals/week2/match_control_flow/src/main.rs
@@ -1,7 +1,11 @@
-fn main() {
-    let name = "Wat's up?";
+use std::io;
 
-    match name {
+fn main() {
+    println!("Please enter a greeting:");
+    let mut greet = String::new();
+    io::stdin().read_line(&mut greet).expect("Failed to read input");
+
+    match greet.trim() {
         "Good Bye" => println!("Sorry to see you go."),
         "Hello" => println!("Hi, nice to meet you!"),
         _ => println!("I can't find a greeting, good bye."),
