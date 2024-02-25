5f826840373d7ed7b66c21aba81b8d250af4e8cf
git-user-cpp <akushyk799@proton.me>
2024-02-25 17:17:28 +0200

    week3_16

--- src/rust_fundamentals/week3/lab_using_enum_as_type/src/main.rs
+++ src/rust_fundamentals/week3/lab_using_enum_as_type/src/main.rs
@@ -1,3 +1,55 @@
+#[derive(Debug)]
+enum WineRegions {
+    Bordeaux,
+    Burgundy,
+    Champagne,
+    Tuscany,
+    Rioja,
+    NapaValley,
+    Khatsapetivka,
+}
+
+struct Wine {
+    name: String,
+    region: WineRegions,
+}
+
+fn supported_regions(w: &WineRegions) {
+    match w {
+        WineRegions::Rioja => println!("Rioja is supported!"),
+        _ => println!("{:?} is not supported!", w),
+    }
+}
+
+fn popularity(w: &WineRegions) {
+    match w {
+        WineRegions::Bordeaux => println!("Highly Popular"),
+        _ => println!("{:?} is not popular!", w),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let wine1 = Wine {
+        name: String::from("Chateau Margaux"),
+        region: WineRegions::Bordeaux,
+    };
+
+    let wine2 = Wine {
+        name: String::from("Barolo"),
+        region: WineRegions::Tuscany,
+    };
+
+    let wine3: Wine = Wine {
+        name: String::from("Domashnie"),
+        region: WineRegions::Khatsapetivka,
+    };
+
+    println!("Wine 1: {} from {:?}", wine1.name, wine1.region);
+    println!("Wine 2: {} from {:?}", wine2.name, wine2.region);
+    println!("Wine 3: {} from {:?}", wine3.name, wine3.region);
+    supported_regions(&wine1.region);
+    supported_regions(&WineRegions::Rioja);
+
+    popularity(&wine1.region);
+    popularity(&wine3.region);
 }
