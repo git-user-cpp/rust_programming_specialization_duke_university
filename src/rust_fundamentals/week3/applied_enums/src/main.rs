9ab9d00aad22e5ed438b41acb4c3721815c9bda7
git-user-cpp <akushyk799@proton.me>
2024-02-25 17:51:18 +0200

    week3_18

--- src/rust_fundamentals/week3/applied_enums/src/main.rs
+++ src/rust_fundamentals/week3/applied_enums/src/main.rs
@@ -1,3 +1,29 @@
+use std::{io::Bytes, result};
+
+enum FileSize {
+    Bytes(u64),
+    KibiBytes(u64),
+    MebiBytes(u64),
+    GibiBytes(u64),
+}
+
+fn format_size(size: u64) -> String {
+    let file_size = match size {
+        0..=1023 => FileSize::Bytes(size),
+        1024..=1_023_999 => FileSize::KibiBytes(size / 1024),
+        1_024_000..=1_023_999_999 => FileSize::GibiBytes(size / 1_024_000),
+        _ => FileSize::GibiBytes(size / 1_024_000_000)
+    };
+
+    match file_size {
+        FileSize::Bytes(bytes) => format!("{} bytes", bytes),
+        FileSize::KibiBytes(kibibytes) => format!("{:.2} kibibytes", kibibytes as f64 / 1024.0),
+        FileSize::MebiBytes(mebibytes) => format!("{:.2} mebibytes", mebibytes as f64 / 1024.0),
+        FileSize::GibiBytes(gibibytes) => format!("{:.2} gibibytes", gibibytes as f64 / 1024.0),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = format_size(63688883700399);
+    println!("{}", result);
 }
