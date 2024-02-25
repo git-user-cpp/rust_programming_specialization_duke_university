3c133939b9a1bd8361155b5dea56df3d7378193b
git-user-cpp <akushyk799@proton.me>
2024-02-25 17:33:05 +0200

    week3_17

--- src/rust_fundamentals/week3/the_option_enum/src/main.rs
+++ src/rust_fundamentals/week3/the_option_enum/src/main.rs
@@ -1,3 +1,21 @@
+fn divide(x: i32, y: i32) -> Option<i32> {
+    if y == 0 {
+        None
+    } else {
+        Some(x / y)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = 10;
+    let b = 2;
+
+    let result: Option<i32> = divide(a, b);
+
+    match result {
+        Some(x) => println!("Result: {}", x),
+        None => println!("Error: division by zero"),
+    }
+
+    println!("Result: {}", result.unwrap());
 }
