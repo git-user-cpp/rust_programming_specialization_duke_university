041163c0300c0f46682fdb786df993636368c7bf
git-user-cpp <akushyk799@proton.me>
2024-02-26 11:48:31 +0200

    week3_21

--- src/rust_fundamentals/week3/lab_using_vectors_with_enums/src/main.rs
+++ src/rust_fundamentals/week3/lab_using_vectors_with_enums/src/main.rs
@@ -1,3 +1,38 @@
+enum Shape {
+    Circle(f64),
+    Square(f64),
+    Triangle(f64, f64),
+}
+
+fn largest(shapes: &Vec<Shape>) {
+    let mut largest_area = 0.0;
+    
+    for shape in shapes {
+        let area = match shape {
+        Shape::Circle(radius) => std::f64::consts::PI * radius * radius,
+        Shape::Square(length) => length * length,
+        Shape::Triangle(base, height) => (base * height) / 2.0,
+        };
+        if area > largest_area {
+            largest_area = area;
+        }
+    }
+    println!("Largest area: {}", largest_area);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let shapes = vec![Shape::Circle(5.0), Shape::Square(3.0), Shape::Triangle(10.0, 5.0)];
+
+    largest(&shapes);
+
+    let total_area: f64 = shapes
+    .iter()
+    .map(|shape| match shape {
+        Shape::Circle(radius) => std::f64::consts::PI * radius * radius,
+        Shape::Square(length) => length * length,
+        Shape::Triangle(base, height) => (base * height) / 2.0,
+    })
+    .sum();
+
+    println!("Total area: {} sq. units", total_area);
 }
