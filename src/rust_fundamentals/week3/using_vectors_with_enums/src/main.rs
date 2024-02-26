7bd6e6455a571edfba06003df819dfee43f970d0
git-user-cpp <akushyk799@proton.me>
2024-02-26 11:32:45 +0200

    week3_20

--- src/rust_fundamentals/week3/using_vectors_with_enums/src/main.rs
+++ src/rust_fundamentals/week3/using_vectors_with_enums/src/main.rs
@@ -1,3 +1,15 @@
+enum Shape {
+    Circle(f64),
+    Square(f64),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let shapes = vec![Shape::Circle(5.0), Shape::Square(3.0), Shape::Circle(2.0)];
+
+    let total_area: f64 = shapes.iter().map(|shape| match shape {
+        Shape::Circle(radius) => std::f64::consts::PI * (radius * radius),
+        Shape::Square(length) => length * length,
+    }).sum();
+
+    println!("Total area: {:.2} sq. units.", total_area);
 }
