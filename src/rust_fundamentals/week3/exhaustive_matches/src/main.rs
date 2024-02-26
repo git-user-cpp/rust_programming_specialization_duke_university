fce963b846b28ce9661f8e295cccb979137c4be7
git-user-cpp <akushyk799@proton.me>
2024-02-26 11:55:56 +0200

    week3_22

--- src/rust_fundamentals/week3/exhaustive_matches/src/main.rs
+++ src/rust_fundamentals/week3/exhaustive_matches/src/main.rs
@@ -1,3 +1,18 @@
+enum WineGrapes {
+    CabernetFranc,
+    Tannat,
+    Merlot,
+}
+
+fn taste_wine(grapes: WineGrapes) {
+    match grapes {
+        WineGrapes::CabernetFranc => println!("This is a Cabernet Franc wine."),
+        _ => println!("This is not a Cabernet Franc wine."),
+        // WineGrapes::Merlot => println!("This is a Merlot wine."),
+        // WineGrapes::Tannat => println!("This is a Tannat wine."),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    taste_wine(WineGrapes::CabernetFranc);
 }
