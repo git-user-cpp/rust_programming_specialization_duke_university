bbad6864db9a3c0107adf5171b8dbed7c1011855
git-user-cpp <akushyk799@proton.me>
2024-02-28 10:35:46 +0200

    data engineering with rust

---
+++ src/data_engineering_with_rust/week1/print_rust_data_structures/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Rust common collections:");
+    
+    println!("\n\tSequences:");
+    println!("\t\tVec");
+    println!("\t\tVecDeque");
+    println!("\t\tLinkedList");
+
+    println!("\n\tMaps:");
+    println!("\t\tHashMap");
+    println!("\t\tBTreeMap");
+
+    println!("\n\tSets:");
+    println!("\t\tHashSet");
+    println!("\t\tBTreeSet");
+
+    println!("\n\tMisc:");
+    println!("\t\tBinaryHeap");
+}
